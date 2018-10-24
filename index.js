62eca2098792817ca8e32b8b58ebd1eaee24503f
luispmenezes <lspmenezes@gmail.com>
2018-10-24 15:44:56 +0100

    Implemented file and directory iteration.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const rp = require('request-promise');
-const base_url = '';
+const $ = require('cheerio');
+const base_url = 'http://';
 const project_name = ''
-const branch = ''
-const target_url = base_url + '/browse/' + project_name + '/' + branch + '/'
+const branch = 'trunk'
+const start_path = '/browse/~br=' + branch + '/' + project_name + '/' + branch
 const session_key = ''
 const session_value = ''
 const session_domain = ''
@@ -10,26 +11,60 @@ const session_domain = ''
 var tough = require('tough-cookie');
 
 let cookie = new tough.Cookie({
-    key: session_key,
-    value: session_value,
-    domain: session_domain,
-    httpOnly: true,
-    maxAge: 31536000
+	key: session_key,
+	value: session_value,
+	domain: session_domain,
+	httpOnly: true,
+	maxAge: 31536000
 });
 
 var cookiejar = rp.jar();
 cookiejar.setCookie(cookie, base_url);
 
-var options = {
-    uri: target_url,
-    jar: cookiejar 
-};
+loadDir(start_path);
 
-rp(options)
-  .then(function(html){
-    //success!
-    console.log(html);
-  })
-  .catch(function(err){
-    //handle error
-  });
+function loadDir(path){
+	console.log(path)
+
+	var options = {
+		uri: base_url+path,
+		jar: cookiejar 
+	};
+
+	rp(options)
+	.then(function(html){
+
+		//iterate files
+		if($('.browse-tree:not(.browse-empty, .browse-directory)', html).length){
+	  		$('.browse-tree:not(.browse-empty, .browse-directory)', html).each(function (i, elem) {
+	  			revision = $(elem).find('.table-row-node-revision>span').attr('data-csid')
+	  			getFileRaw($(elem).find('.table-row-node-name>a').attr('href'),revision);
+	  		});
+	  	}
+
+	  	//iterate directories
+	  	if($('.browse-directory:not(.browse-empty)>.table-row-node-name>a', html).length){
+	  		$('.browse-directory:not(.browse-empty)>.table-row-node-name>a', html).each(function (i, elem) {
+	  			loadDir($(elem).attr('href'));
+	  		});
+	  	}
+	  })
+	.catch(function(err){
+	    console.log(err)
+	});
+}
+
+function getFileRaw(path,revision){
+	console.log(path)
+
+	var options = {
+		uri: base_url+path.replace('~br='+branch,'~raw,r='+revision),
+		jar: cookiejar 
+	};
+
+	rp(options)
+	.then(function(html){return html;})
+	.catch(function(err){
+		console.log(options.uri)
+	});	
+}
