7bb153f64c64449dd96f1fab0f183d03403a6726
luispmenezes <lspmenezes@gmail.com>
2018-10-24 17:50:09 +0100

    Implemented local saving, some files still broken.

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const rp = require('request-promise');
 const $ = require('cheerio');
-const base_url = 'http://';
+const fs = require('fs');
+const del = require('del');
+
+const base_url = '';
 const project_name = ''
-const branch = 'trunk'
+const branch = ''
 const start_path = '/browse/~br=' + branch + '/' + project_name + '/' + branch
 const session_key = ''
 const session_value = ''
 const session_domain = ''
+const output_dir = './output';
 
 var tough = require('tough-cookie');
 
@@ -21,11 +25,20 @@ let cookie = new tough.Cookie({
 var cookiejar = rp.jar();
 cookiejar.setCookie(cookie, base_url);
 
+if (!fs.existsSync(output_dir)){
+	fs.mkdirSync(output_dir);
+}else{
+	del.sync([output_dir+'/*'])
+}
+
+var project_dir = output_dir + '/' + project_name;
+fs.mkdirSync(project_dir);
+var branch_dir = project_dir + '/' + branch;
+fs.mkdirSync(branch_dir);
+
 loadDir(start_path);
 
 function loadDir(path){
-	console.log(path)
-
 	var options = {
 		uri: base_url+path,
 		jar: cookiejar 
@@ -36,35 +49,53 @@ function loadDir(path){
 
 		//iterate files
 		if($('.browse-tree:not(.browse-empty, .browse-directory)', html).length){
-	  		$('.browse-tree:not(.browse-empty, .browse-directory)', html).each(function (i, elem) {
-	  			revision = $(elem).find('.table-row-node-revision>span').attr('data-csid')
-	  			getFileRaw($(elem).find('.table-row-node-name>a').attr('href'),revision);
-	  		});
-	  	}
+			$('.browse-tree:not(.browse-empty, .browse-directory)', html).each(function (i, elem) {
+				var revision = $(elem).find('.table-row-node-revision>span').attr('data-csid');
+				var urlPath = $(elem).find('.table-row-node-name>a').attr('href');
+
+				var filePath = output_dir + urlPath.substring(urlPath.indexOf("/",9));
+				var fileContent = getFileRaw(urlPath,revision);
+
+				fs.writeFile(filePath, fileContent, function(err) {
+				    if(err) {
+				        throw err;
+				    }
+				    console.log('F: ' + filePath)
+				}); 
+
+			});
+		}
 
 	  	//iterate directories
 	  	if($('.browse-directory:not(.browse-empty)>.table-row-node-name>a', html).length){
 	  		$('.browse-directory:not(.browse-empty)>.table-row-node-name>a', html).each(function (i, elem) {
-	  			loadDir($(elem).attr('href'));
+	  			var urlPath = $(elem).attr('href');
+	  			
+	  			var dirPath = output_dir + urlPath.substring(urlPath.indexOf("/",9));
+	  			fs.mkdirSync(dirPath);
+
+				loadDir(urlPath);
+				console.log('D: ' + dirPath)
 	  		});
 	  	}
 	  })
 	.catch(function(err){
-	    console.log(err)
+		throw err;
 	});
 }
 
 function getFileRaw(path,revision){
-	console.log(path)
-
 	var options = {
 		uri: base_url+path.replace('~br='+branch,'~raw,r='+revision),
-		jar: cookiejar 
+		jar: cookiejar,
+		method: "GET",
+  		encoding: "binary" 
 	};
 
 	rp(options)
-	.then(function(html){return html;})
+	.then(function(body,data){
+		return body;})
 	.catch(function(err){
-		console.log(options.uri)
+		throw err;
 	});	
 }
