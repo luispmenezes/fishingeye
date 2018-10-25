02efcc99283c368d110bb0913be52599f357c672
luispmenezes <lspmenezes@gmail.com>
2018-10-25 13:57:22 +0100

    Fixed binary file extraction.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const del = require('del');
 const base_url = '';
 const project_name = ''
 const branch = ''
-const start_path = '/browse/~br=' + branch + '/' + project_name + '/' + branch
+const start_path = `/browse/~br=${branch}/${project_name}/${branch}`
 const session_key = ''
 const session_value = ''
 const session_domain = ''
@@ -28,12 +28,12 @@ cookiejar.setCookie(cookie, base_url);
 if (!fs.existsSync(output_dir)){
 	fs.mkdirSync(output_dir);
 }else{
-	del.sync([output_dir+'/*'])
+	del.sync([`${output_dir}/*`])
 }
 
-var project_dir = output_dir + '/' + project_name;
+var project_dir = `${output_dir}/${project_name}`;
 fs.mkdirSync(project_dir);
-var branch_dir = project_dir + '/' + branch;
+var branch_dir = `${project_dir}/${branch}`;
 fs.mkdirSync(branch_dir);
 
 loadDir(start_path);
@@ -53,16 +53,7 @@ function loadDir(path){
 				var revision = $(elem).find('.table-row-node-revision>span').attr('data-csid');
 				var urlPath = $(elem).find('.table-row-node-name>a').attr('href');
 
-				var filePath = output_dir + urlPath.substring(urlPath.indexOf("/",9));
-				var fileContent = getFileRaw(urlPath,revision);
-
-				fs.writeFile(filePath, fileContent, function(err) {
-				    if(err) {
-				        throw err;
-				    }
-				    console.log('F: ' + filePath)
-				}); 
-
+				getFileRaw(urlPath,revision);
 			});
 		}
 
@@ -74,8 +65,8 @@ function loadDir(path){
 	  			var dirPath = output_dir + urlPath.substring(urlPath.indexOf("/",9));
 	  			fs.mkdirSync(dirPath);
 
-				loadDir(urlPath);
-				console.log('D: ' + dirPath)
+	  			loadDir(urlPath);
+	  			console.log('D: ' + dirPath)
 	  		});
 	  	}
 	  })
@@ -86,15 +77,26 @@ function loadDir(path){
 
 function getFileRaw(path,revision){
 	var options = {
-		uri: base_url+path.replace('~br='+branch,'~raw,r='+revision),
+		uri: base_url+path.replace(`~br=${branch}`,`~raw,r=${revision}`),
 		jar: cookiejar,
 		method: "GET",
-  		encoding: "binary" 
+		encoding: null
 	};
 
 	rp(options)
 	.then(function(body,data){
-		return body;})
+		var filePath = output_dir + path.substring(path.indexOf("/",9));
+
+		console.log(options.uri)
+
+
+		fs.writeFile(filePath, body, function(err) {
+			if(err) {
+				throw err;
+			}
+			console.log('F: ' + filePath)
+		}); 
+	})
 	.catch(function(err){
 		throw err;
 	});	
